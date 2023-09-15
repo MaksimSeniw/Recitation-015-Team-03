41e7a30e3a821137b8eb4971f07fa3aab6ba2c84
MaksimSeniw <mase5436@colorado.edu>
2023-09-15 08:53:46 -0600

    Version - <Person1> : <return value>

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 std::vector<bool> LessMask(std::vector<double> nums, double less_than);
 std::vector<bool> GreaterMask(std::vector<int> nums, double greater_than);
-
+#redo
 int main()
 {
     return 1;
